fff29e4bae986d74ac7ad1849d6abe70087a4227
Santiago Piccinini (SAn) <piccinini.santiago@gmail.com>
2011-08-13 19:00:36 -0300

    Added decoder

--- celp.py
+++ celp.py
@@ -7,20 +7,9 @@ from scipy.signal import lfilter
 import scipy.linalg
 from itertools import islice
 
+import lpc
 
 SAMPLE_RATE = 8000
-#FRAME_LENGTH = 160 # 0.02s @8000Hz
-#N_SUBFRAMES = 4
-#SUBFRAME_LENGTH =  FRAME_LENGTH / N_SUBFRAMES
-
-#LPC_ORDER = 10
-
-#FC_SIZE = 128
-
-#ZERO_INPUT = np.zeros(FRAME_LENGTH)
-
-#DELTA = np.concatenate(([1.], np.zeros(SUBFRAME_LENGTH-1)))
-
 
 # TODO:
 #
@@ -160,76 +149,3 @@ def search_codebook(M, d, codebook):
                                                               best_codevector)
 
     return max_index, best_amplification
-
-"""
-SR, signal = scipy.io.wavfile.read('./data/mike_8.wav')
-signal = signal.astype("float64")
-signal /= np.max(signal)
-assert(SR==8000)
-
-
-signal = np.concatenate((signal, np.zeros(FRAME_LENGTH - (len(signal) % FRAME_LENGTH))))
-signal = signal.reshape((len(signal)/FRAME_LENGTH, FRAME_LENGTH))
-
-frames = signal
-
-last_excitation_code = np.zeros(SUBFRAME_LENGTH)
-
-out_amplifs = []
-out_lpc_coefs = []
-out_fc_index = []
-
-# Encoding
-
-for frame in frames:
-    lpc_error_coeffs = lpc.lpc_ref(frame, LPC_ORDER)
-    out_lpc_coefs.append(lpc_error_coeffs)
-    # Buid the H matrix
-    h = lfilter([1], lpc_error_coeffs, DELTA)
-    H = scipy.linalg.toeplitz(h, np.concatenate(([h[0]], ZERO_INPUT[:SUBFRAME_LENGTH-1])))
-
-    for subframe in frame.reshape((N_SUBFRAMES, SUBFRAME_LENGTH)):
-        z0 = lfilter([1], lpc_error_coeffs,
-                     np.concatenate((last_excitation_code, np.zeros(SUBFRAME_LENGTH))))[SUBFRAME_LENGTH:]
-        M = H
-        d = subframe - z0
-        index, amplif = search_codebook(M, d, fixed_codebook)
-
-        last_excitation_code = amplif * fixed_codebook[index]
-        #pylab.plot(subframe)
-        #pylab.plot(np.dot(H, last_excitation_code)+z0)
-
-        out_amplifs.append(amplif)
-        out_fc_index.append(index)
-
-print out_amplifs
-print out_fc_index
-print out_lpc_coefs
-
-# Decoding
-
-out_signal = np.array([])
-excitation = np.zeros(SUBFRAME_LENGTH)
-
-amplifs, fc_indexs = iter(out_amplifs), iter(out_fc_index)
-
-for lpc_error_coeffs in out_lpc_coefs:
-
-    h = lfilter([1], lpc_error_coeffs, DELTA)
-    H = scipy.linalg.toeplitz(h, np.concatenate(([h[0]], ZERO_INPUT[:SUBFRAME_LENGTH-1])))
-
-    for subframe in range(4):
-        z0 = lfilter([1], lpc_error_coeffs,
-                     np.concatenate((excitation, np.zeros(SUBFRAME_LENGTH))))[SUBFRAME_LENGTH:]
-
-        amplif, index  = amplifs.next(), fc_indexs.next()
-        excitation = amplif * fixed_codebook[index]
-
-        out = np.dot(H, excitation) + z0
-
-        out_signal = np.concatenate((out_signal, out))
-
-out_signal = out_signal * np.iinfo(np.int16).max
-scipy.io.wavfile.write('./data/mike_8_out.wav', 8000, out_signal.astype("int16"))
-
-"""
--- decoder.py
+++ decoder.py
@@ -10,10 +10,10 @@ from ctsndfile.libsndfile import SndFile, OPEN_MODES, FILE_FORMATS
 
 import celp
 
-parser = argparse.ArgumentParser(description='CELP encoder.')
-parser.add_argument('-i', type=str, default="-",
+parser = argparse.ArgumentParser(description='CELP decoder.')
+parser.add_argument('-i', type=argparse.FileType('r'), default=sys.stdin,
                     help='input file. Defaults to standard input')
-parser.add_argument('-o', type=argparse.FileType('w'), default=sys.stdout,
+parser.add_argument('-o', type=str, default="-",
                     help='output file. Defaults to standard output')
 
 parser.add_argument('--lpc-order', type=int, default=10,
@@ -27,28 +27,26 @@ parser.add_argument('--fixed-cb-size', type=int, default=4,
 
 args = parser.parse_args()
 
+in_file = args.i
 
-in_file = SndFile(args.i)
+out_file = SndFile(args.o, open_mode=OPEN_MODES.SFM_WRITE,
+                   writeSamplerate=8000,
+                   writeFormat=FILE_FORMATS.SF_FORMAT_WAV|FILE_FORMATS.SF_FORMAT_PCM_16,
+                   writeNbChannels=1)
 
-assert in_file.samplerate == 8000
-assert in_file.channels == 1
-
-out_file = args.o
-
-celp_encoder = celp.CELP(frame_length=args.frame_length,
+celp_decoder = celp.CELP(frame_length=args.frame_length,
                          n_subframes=args.subframes,
                          lpc_order=args.lpc_order,
                          fixed_codebook_size=args.fixed_cb_size)
 
-bytes_per_frame = celp_encoder.bytes_per_frame()
+bytes_per_frame = celp_decoder.bytes_per_frame()
 
 while True:
-    frame, n_samples = in_file.read(args.frame_length, dtype="float64")
-
-    if n_samples < args.frame_length:
+    frame_bits = in_file.read(bytes_per_frame)
+    if len(frame_bits) < bytes_per_frame:
         break
 
-    bits = celp_encoder.encode(frame[:,0])
-    out_file.write(bits)
+    frame = celp_decoder.decode(frame_bits)
+    out_file.write(frame)
 
 out_file.close()
